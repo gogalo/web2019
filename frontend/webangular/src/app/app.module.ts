e2e6a8c2d190be7a540a610dbfd335d136ce10a0
Zuriñe Moreno <zurizu88@gmail.com>
2019-04-08 17:34:51 +0200

    users+posts

--- frontend/webangular/src/app/app.module.ts
+++ frontend/webangular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { PublicacionesComponent } from './publicaciones/publicaciones.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuariosComponent,
+    PublicacionesComponent
   ],
   imports: [
     BrowserModule,
