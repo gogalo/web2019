e4417ee40c099baa405843b96707adf7f5ba110e
Zuriñe Moreno <zurizu88@gmail.com>
2019-04-09 18:02:48 +0200

    implementan2 rutas

--- frontend/webangular/src/app/app.module.ts
+++ frontend/webangular/src/app/app.module.ts
@@ -7,18 +7,30 @@ import { AppComponent } from './app.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
 import { PublicacionesComponent } from './publicaciones/publicaciones.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+const routes: Routes = [{ path: 'usuarios', component: UsuariosComponent },
+ { path: 'home', component: HomeComponent },
+ { path: 'request', component: RequestComponent },
+ { path: ''
+, redirectTo: '/home', pathMatch: 'full' }, 
+]; 
 @NgModule({
   declarations: [
     AppComponent,
     UsuariosComponent,
-    PublicacionesComponent
+    PublicacionesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
 	AngularImportsModule,
-	BrowserAnimationsModule
+	BrowserAnimationsModule,
+	RouterModule.forRoot(
+ 	appRoutes,
+ { enableTracing: true } // <-- debugging purposes only
+ ) ,
   ],
   providers: [],
   bootstrap: [AppComponent]
